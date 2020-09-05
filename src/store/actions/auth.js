7d47db187948fde94a2da74b93db49f8fcfcfcb8
gokartgo <gokart.007@hotmail.com>
2020-09-06 00:07:13 +0700

    add spinner 329

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
 	}
 }
 
-export const authSuccess = authData => {
+export const authSuccess = (idToken, userId) => {
 	return {
 		type: actionTypes.AUTH_SUCCESS,
-		authData,
+		idToken,
+		userId,
 	}
 }
 
@@ -35,10 +36,9 @@ export const auth = (email, password, isSignup) => dispatch => {
 		.post(url, authData)
 		.then(response => {
 			console.log(response)
-			dispatch(authSuccess(response.data))
+			dispatch(authSuccess(response.data.idToken, response.data.localId))
 		})
-		.catch(err => {
-			console.log(err)
-			dispatch(authFail())
+		.catch(({ response }) => {
+			dispatch(authFail(response.data.error))
 		})
 }

ccd4abac94d80297015f1002dce758c69ffba88d
gokartgo <gokart.007@hotmail.com>
2020-09-06 16:00:46 +0700

    finish lecture 334

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import Spinner from '../../components/UI/Spinner/Spinner'
@@ -125,8 +127,14 @@ class Auth extends Component {
 			errorMessage = <p>{this.props.error.message}</p>
 		}
 
+		let authRedirect = null
+		if (this.props.isAuthenticationed) {
+			authRedirect = <Redirect to='/' />
+		}
+
 		return (
 			<div className={classes.Auth}>
+				{authRedirect}
 				{errorMessage}
 				<form onSubmit={this.submitHandler}>
 					{form}
@@ -144,6 +152,7 @@ const mapStateToProps = state => {
 	return {
 		loading: state.auth.loading,
 		error: state.auth.error,
+		isAuthenticationed: state.auth.token !== null,
 	}
 }
 
